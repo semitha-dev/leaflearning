5136feacbc2d02ecfaf2b0cce768d22596d8913d
semitha-dev <villaddarage@gmail.com>
2025-04-06 21:57:58 +0530

    Jamesweb

--- learlearning/app/layout.tsx
+++ learlearning/app/layout.tsx
@@ -24,9 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        {/* Google AdSense Script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3641349574273554"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
