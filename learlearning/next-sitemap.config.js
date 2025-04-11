ae44030e2533a418b34b2aa2ddd219286c7745d2
semitha-dev <villaddarage@gmail.com>
2025-04-11 23:00:15 +0530

    sd

---
+++ learlearning/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://www.leaflearning.me", // 🔁 change to your actual domain
+    generateRobotsTxt: true, // this also creates robots.txt
+  };
+  
