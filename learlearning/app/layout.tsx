2155e7121b0d31b14b0254d9a6e4132e220cbd3a
semitha-dev <villaddarage@gmail.com>
2025-04-11 22:46:30 +0530

    dw

--- learlearning/app/layout.tsx
+++ learlearning/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -84,6 +84,7 @@ export default function RootLayout({
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <Analytics /> {/* <--- Add this here */}
       </body>
     </html>
   );
